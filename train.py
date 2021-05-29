70f67bd69da8ebe81fbda24423adbd0ee028fa8b
Emma163 <79447215+Emma163@users.noreply.github.com>
2021-05-29 14:59:21 +0200

    Update train.py

--- train.py
+++ train.py
@@ -210,7 +210,7 @@ if __name__ == '__main__':
         '--num_workers', '8',
         '--num_classes', '21',
         '--cuda', '0',
-        '--batch_size', '32',
+        '--batch_size', '64',
         '--save_model_path', './checkpoints_18_sgd',
         '--context_path', 'resnet18',  # set resnet18 or resnet101, only support resnet18 and resnet101
         '--optimizer', 'sgd',
